e21c90384bc057c4a1af9ac35accc978421835cc
Jinny268 <101381590+Jinny268@users.noreply.github.com>
2022-06-17 00:04:22 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 #컴퓨터공학부 202103811 황혜진
 
 print('학교: 한국외국어대학교')
-print('학과: 컴퓨터공학부')
+print('학과: 컴퓨터공학부(Computer Engineering)')
 print('학번: 202103811')
 print('학년: 2학년')
 print('이름: 황혜진')
 print('취미: 음악듣기, 산책하기, 영화보기')
-print('Computer Engineering') # Touch by HwangBBang (2022-03-28)
 
 h="Hello "
 b="Good bye "
